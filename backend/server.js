e6d081be525c4eecc49641bfb50fdf8ab65b9125
mysteryn3554 <nikhil.mishra.1704@gmail.com>
2025-02-27 12:50:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ import { connectDB } from "./db/connectDB.js";
 import { configurePassport } from "./passport/passport.config.js";
 import path from "path";
 
-const job = require("./cronJob.js");
+import { job } from "./cronJob.js";
 
 dotenv.config();
 configurePassport();
